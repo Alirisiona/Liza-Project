8603511cc6046c5ea78de4897c65565e9c4fe751
Alirisiona <ALIRISIONA@GMAIL.COM>
2024-07-10 21:09:54 +0300

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,7 @@
+#"4th program"
+print (int(13.42))
+print (int(42.13))
+print (int((13.42 - int(13.42))*100 + 1))
+print (int((42.13 - int(42.13))*100))
+print ((int(13.42)) == int((42.13 - int(42.13))*100))
+print ((int(42.13)) == int((13.42 - int(13.42))*100 + 1))
